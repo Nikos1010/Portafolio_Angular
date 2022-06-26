64ebe72efc1a953e8476db7b9ba8badc8921715e
Noith <nimoba101@gmail.com>
2022-06-26 00:10:14 -0500

    Fin de la seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,16 +9,38 @@ export class ProductosService {
 
   cargando: boolean = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
     this.cargarProducto();
   }
 
   private cargarProducto() {
-    this.http.get('https://angular-html-4ea23-default-rtdb.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: any) => {
-        this.productos = resp;
-        this.cargando = false;
-      });
+    return new Promise<void>( (resolve, reject) => {
+      this.http.get('https://angular-html-4ea23-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: any) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
+    });
+  }
+
+  getProducto (id: string) {
+    return this.http.get(`https://angular-html-4ea23-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string){
+    if(this.productos.length === 0){
+      this.cargarProducto().then(() => this.filtrarProductos(termino));
+    } else {
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino: string) {
+    termino = termino.toLowerCase();
+    this.productosFiltrado = this.productos.filter(prod => prod.categoria?.includes(termino) || prod.titulo?.toLowerCase().includes(termino));
+    console.log(this.productosFiltrado);
   }
 }
