65bcf2a320945a86d3c60d94eed88fc1ac8af623
Noith <nimoba101@gmail.com>
2022-06-25 21:22:48 -0500

    Fin de la seccion 6

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando: boolean = true;
+  productos: Producto[] = [];
+
+  constructor( private http: HttpClient ) { 
+    this.cargarProducto();
+  }
+
+  private cargarProducto() {
+    this.http.get('https://angular-html-4ea23-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: any) => {
+        this.productos = resp;
+        this.cargando = false;
+      });
+  }
+}
